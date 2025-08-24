1727f6c0804b767ce7657dd485c38c921793b556
AtisangInn <578020@student.belgiumcampus.ac.za>
2025-08-24 19:47:28 +0200

    refactor: Update Algorithm interface to return SolutionResult

--- LPR381/Algorithm.cs
+++ LPR381/Algorithm.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace LPR381
 {
-	public interface Algorithm
-	{
-		void Solve();
-	}
+    public interface Algorithm
+    {
+        SolutionResult Solve();
+    }
 }
